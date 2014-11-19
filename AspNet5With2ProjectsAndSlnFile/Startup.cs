5e2a01c5b3638e4d12cf71498c17a5c97ab97c3e
Ahmed ElSayed <ahmels@microsoft.com>
2014-11-19 12:25:46 -0800

    initial

---
+++ AspNet5With2ProjectsAndSlnFile/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using ClassLibrary;
+
+namespace AspNet5With2ProjectsAndSlnFile
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async ctx =>
+            {
+                ctx.Response.ContentType = "text/plain";
+                await ctx.Response.WriteAsync(Class1.WelcomeMessage);
+            });
+        }
+    }
+}
---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static string WelcomeMessage
+        {
+            get
+            {
+                return "Welcome from ClassLibrary"
+            }
+        }
+    }
+}
